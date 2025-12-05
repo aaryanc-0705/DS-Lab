9d258194cb71aca0ef6f59a0ecf63f0c18a27f7b
Aaryan Chauhan <aaryanc0705@gmail.com>
2025-12-05 07:02:09 +0530

    Add files via upload

---
+++ DSA Assignment 9/BFS.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+
+void bfs(int graph[][6], int start, int n) {
+    bool visited[6] = {false};
+    int queue[6];
+    int front = 0, rear = 0;
+
+    visited[start] = true;
+    queue[rear++] = start;
+
+    cout << "Source vertex for traversal: " << start << endl;
+
+    while (front < rear) {
+        int current = queue[front++];
+        cout << current << " ";
+
+        for (int i = 0; i < n; i++) {
+            if (graph[current][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                queue[rear++] = i;
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main(){
+    int n, start;
+    int graph[6][6];
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    cout << "Enter adjacency matrix:\n";
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> graph[i][j];
+        }
+    }
+    cout << "Enter starting vertex for BFS (0 to " << n-1 << "): ";
+    cin >> start;
+    bfs(graph, start, n);
+    return 0;
+}
